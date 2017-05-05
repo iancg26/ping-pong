2658a862783d591a7f00d3e97da8341da8ba303c
Ian Garner <icgarner21@gmail.com>
2017-05-05 11:27:02 -0700

    add the backend logic including a for loop with if and else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+// backend logic
+var pongGame = [];
+var pingpong = function(n){
+  for (var i = 0; i <= n; i++){
+  if (i % 3 === 0){
+    pongGame.push("Ping");
+  }
+  else if (i % 5 === 0){
+    pongGame.push("Pong");
+  }
+
+  else if (i % 15 === 0){
+    pongGame.push("Ping Pong");
+  }
+  else{
+    pongGame.push('' + i + '');
+  }
+  }
+};
+
+
+
+
+// front end logic
 $(document).ready(function() {
   $("form#Ping-pong").submit(function(event){
 
